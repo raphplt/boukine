2ffc4f460121576b4e088f8acdcd40934bcef2de
raphaeltkorp <raphael@tkorp.com>
2025-09-24 21:28:08 +0200

    chore: update dependencies and add Tailwind CSS configuration
    - Added new dependencies: @expo/html-elements, @gluestack-ui/core, @gluestack-ui/utils, @legendapp/motion, babel-plugin-module-resolver, nativewind, react-aria, react-native-svg, react-stately, tailwind-variants, and tailwindcss.
    - Updated react-native-safe-area-context to version 5.6.1.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Updated tsconfig.json to include tailwind.config.js and nativewind-env.d.ts in paths.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,34 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Box } from "@/components/ui/box";
+import { Spinner } from "@/components/ui/spinner";
 
 export default function HomeScreen() {
-  return (
-			<ParallaxScrollView
-				headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-				headerImage={
-					<Image
-						source={require("@/assets/images/partial-react-logo.png")}
-						style={styles.reactLogo}
-					/>
-				}
-			>
-				<ThemedView style={styles.titleContainer}>
-					<ThemedText type="title">Boukine</ThemedText>
-					<HelloWave />
-				</ThemedView>
-				<ThemedText type="title">Noter un livre</ThemedText>
-			</ParallaxScrollView>
-		);
+	return (
+		<ParallaxScrollView
+			headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+			headerImage={
+				<Image
+					source={require("@/assets/images/partial-react-logo.png")}
+					style={styles.reactLogo}
+				/>
+			}
+		>
+			<ThemedView style={styles.titleContainer}>
+				<ThemedText type="title">Boukine</ThemedText>
+				<HelloWave />
+			</ThemedView>
+			<ThemedText type="title">Noter un livre</ThemedText>
+
+			<Box />
+			<Spinner size="large" />
+		</ParallaxScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
