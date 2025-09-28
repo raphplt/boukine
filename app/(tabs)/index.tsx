7895075de66f6175eabd499f15800da9ecdcf198
raphaeltkorp <raphael@tkorp.com>
2025-09-28 21:56:14 +0200

    feat: integrate expo-camera for QR code scanning and update app configuration
    - Added expo-camera dependency for camera functionality.
    - Introduced QrCodeScanner component to handle QR code scanning.
    - Updated app.json to include camera permissions and modified splash screen configuration.
    - Removed unused images and updated existing image assets.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,47 +1,35 @@
-import { Image } from 'expo-image';
-import { StyleSheet } from "react-native";
-
-import { HelloWave } from "@/components/hello-wave";
-import ParallaxScrollView from "@/components/parallax-scroll-view";
+import QrCodeScanner from "@/components/common/QrCodeScanner";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import { StyleSheet } from "react-native";
 
 export default function HomeScreen() {
 	return (
-		<ParallaxScrollView
-			headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-			headerImage={
-				<Image
-					source={require("@/assets/images/partial-react-logo.png")}
-					style={styles.reactLogo}
-				/>
-			}
-		>
+		<>
 			<ThemedView style={styles.titleContainer}>
 				<ThemedText type="title">Boukine</ThemedText>
-				<HelloWave />
 			</ThemedView>
 			<ThemedText type="title">Noter un livre</ThemedText>
-
-		</ParallaxScrollView>
+			<QrCodeScanner />
+		</>
 	);
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+	titleContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 8,
+	},
+	stepContainer: {
+		gap: 8,
+		marginBottom: 8,
+	},
+	reactLogo: {
+		height: 178,
+		width: 290,
+		bottom: 0,
+		left: 0,
+		position: "absolute",
+	},
 });
